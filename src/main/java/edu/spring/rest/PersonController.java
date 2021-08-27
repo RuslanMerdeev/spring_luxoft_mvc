7032db7f91ced1d5e286e7d7ffe70406f1b1f31f
merdeevrz <merdeevrz@haulmont.com>
2021-08-27 15:35:28 +0400

    Added the methods to controller

--- src/main/java/edu/spring/rest/PersonController.java
+++ src/main/java/edu/spring/rest/PersonController.java
@@ -1,6 +1,5 @@
 package edu.spring.rest;
 
-import edu.spring.domain.Person;
 import edu.spring.repostory.PersonRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -17,5 +16,47 @@ public class PersonController {
     @Autowired
     public PersonController(PersonRepository repository) {
         this.repository = repository;
+    }
+
+    @GetMapping("/persons")
+    List<PersonDto> getPersons() {
+        return repository.findAll()
+                .stream()
+                .map(PersonDto::toDto)
+                .collect(Collectors.toList());
+    }
+
+    @GetMapping("/person/{id}")
+    PersonDto getPersonById(@PathVariable int id) {
+        return repository.findById(id)
+                .map(PersonDto::toDto)
+                .orElse(null);
+    }
+
+    @PostMapping("/person")
+    PersonDto postPerson(@RequestBody PersonDto person) {
+        if (!repository.existsById(person.getId())) {
+            repository.save(PersonDto.toDomainObject(person));
+            return person;
+        }
+        return null;
+    }
+
+    @PutMapping("/person")
+    PersonDto putPerson(@RequestBody PersonDto person) {
+        if (repository.existsById(person.getId())) {
+            repository.deleteById(person.getId());
+        }
+        repository.save(PersonDto.toDomainObject(person));
+        return person;
+    }
+
+    @DeleteMapping("/person/{id}")
+    ResponseEntity<String> deletePersonById(@PathVariable int id) {
+        if (repository.existsById(id)) {
+            repository.deleteById(id);
+            return ResponseEntity.ok("ok");
+        }
+        return ResponseEntity.notFound().build();
     }
 }
